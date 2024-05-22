9f1b695bf6bbffc52c4822602a0866a83ad2ee1e
PARASMANIKHUNTE <137080546+PARASMANI-KHUNTE@users.noreply.github.com>
2024-05-22 14:51:33 +0530

    Files uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        '9vh': '9vh', // Add custom height
+      },
+    },
+  },
+  plugins: [],
+}
